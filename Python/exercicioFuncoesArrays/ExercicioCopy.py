5af91efbf4dcf0dfb7abf2e5b408966b31427f24
Rafael Altaf dos Santos <104704833+RafaelAltaf@users.noreply.github.com>
2022-07-21 17:52:28 -0300

    Update ExercicioCopy.py

--- Python/exercicioFuncoesArrays/ExercicioCopy.py
+++ Python/exercicioFuncoesArrays/ExercicioCopy.py
@@ -1,7 +1,7 @@
 nomes = ['Rafael', 'Altaf', 'da', 'Silva', 'Pinheiro', 'de', 'Jesus']
 
 def Copy(lista):
-    copia = list(lista)
+    copia += list(lista)
     return copia
 nomes2 = Copy(nomes)
 print(nomes2)
